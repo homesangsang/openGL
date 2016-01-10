013f1d4a79012b8651d05e525099048968a3089a
homesangsang <homesangsang@users.noreply.github.com>
2016-01-10 19:00:20 +0800

    Update Vertex_Line.cpp

--- Vertex_Line.cpp
+++ Vertex_Line.cpp
@@ -9,6 +9,16 @@ void drawVertex(void)
 		代码分析：
 			glBegin()函数是一个图形区域的开始，glEnd()函数是一个图形区域的结束，中间的glVertex*函数是确定的点
 			确定的点必须两两一组，确定一条直线，glVertex的参数是点的坐标
+
+		glBegin函数的参数：
+			GL_POINTS:只在坐标中表示出来指定的点
+			GL_LINES:每次连接两个点（一组）
+			GL_LINE_STRIP:依次连接出现的点
+			GL_LINE_LOOP:依次首尾连接出现的点
+			GL_TRIANGLES:首尾连接之后填充连接起来的区域
+			GL_TRIANGLE_STRIP:...
+			GL_TRIANGLE_FAN
+
 	*/
 	glBegin(GL_LINES);
 		glVertex2f(0.0f,0.0f);
