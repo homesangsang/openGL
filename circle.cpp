9a2a1d7487f535d2c9d6d7920c440488cf165bcb
homesangsang <homesangsang@users.noreply.github.com>
2016-01-10 19:41:54 +0800

    Update circle.cpp
    Add beautification function

--- circle.cpp
+++ circle.cpp
@@ -7,6 +7,13 @@ const GLfloat PI = 3.1415926536f;
 void init(void)
 {
 	int i;
+	
+	glEnable(GL_BLEND);             //启用混合功能，将图形颜色同周围颜色相混合  
+        glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);  
+        glEnable(GL_POINT_SMOOTH);       //点抗锯齿  
+        glEnable(GL_LINE_SMOOTH);        //线抗锯齿  
+        glEnable(GL_POLYGON_SMOOTH);     //多边形抗锯齿  
+	
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_POLYGON);
 	for(i = 0 ; i < n ; i ++ )
